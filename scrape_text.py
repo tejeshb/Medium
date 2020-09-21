ee5c216b51655c0f86ea7f9f9002b7703d6b5b3f
Tejesh.ai <teja.tejeshteja@gmail.com>
2020-09-21 19:46:01 +0100

    Add files via upload

---
+++ scrape_text.py
@@ -0,0 +1,6 @@
+urls = ["https://medium.com/huggingface/how-to-build-a-state-of-the-art-conversational-ai-with-transfer-learning-2d818ac26313?source=tag_archive---------0-----------------------",
+        "https://medium.com/huggingface/distilbert-8cf3380435b5?source=tag_archive---------0-----------------------",
+        "https://medium.com/turing-talks/como-machine-learning-consegue-diferenciar-heter%C3%B4nimos-de-fernando-pessoa-156d0d52a478?source=tag_archive---------0-----------------------",
+        "https://medium.com/@xenolytix/a-future-of-better-care-and-better-health-a-value-based-journey-with-natural-language-processing-c91474800f9e?source=tag_archive---------3-----------------------",
+        "https://medium.com/rakuten-rapidapi/top-10-best-text-analytics-apis-watson-google-natural-language-and-more-b67907b9ba9f?source=tag_archive---------1-----------------------"]
+
